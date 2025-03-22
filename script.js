fc80feb6a4abbd621d982b341ea73658907c15a1
anubhavpathak03 <pathak.anubhavdtu@gmail.com>
2025-03-22 15:26:39 +0530

    complete Static UI part

---
+++ script.js
@@ -0,0 +1,47 @@
+let row = 100;
+let col = 26;
+
+let representColContainer = document.querySelector(".represent-col-container");
+for(let i=0; i<100; i++) {
+    let addressRow = document.createElement("div");
+    addressRow.innerText = i+1;
+    addressRow.setAttribute("class", "address-each-row")
+    representColContainer.appendChild(addressRow)
+}
+
+let representrowcontainer = document.querySelector(".represent-row-container");
+for(let i=0; i<26; i++) {
+    let addressCol= document.createElement("div");
+    // 65 -> A, 66->B, 67->C
+    addressCol.innerText = String.fromCharCode(65 + i);
+    addressCol.setAttribute("class", "address-each-col");
+    representrowcontainer.appendChild(addressCol)
+}
+
+
+
+let cellContainer = document.querySelector(".cell-container");
+for(let i = 0; i < row; i++) {
+    let rowContainer = document.createElement("div");
+    rowContainer.setAttribute("class", "about-rowContainer")
+    for(let j = 0; j < col; j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "about-cell");
+        cell.setAttribute("contenteditable", "true");
+        rowContainer.appendChild(cell);
+        addingListenerForAddressBarDisplay(cell, i, j);
+    }    
+
+    cellContainer.appendChild(rowContainer);
+}
+
+
+let addressBar = document.querySelector(".address-bar");
+
+function addingListenerForAddressBarDisplay(cell, i, j) {
+    cell.addEventListener("click", (e) => {
+        let rowId = i+1;
+        let colId = String.fromCharCode(65 + j);
+        addressBar.value = `${colId}${rowId}`
+    });
+}
